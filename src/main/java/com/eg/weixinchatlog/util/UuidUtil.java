c0730a1029e9447de12b18d72593bfa08be66cbd
makewheels <33256842+makewheels@users.noreply.github.com>
2020-02-16 21:31:20 +0800

    开始解决silk转码

---
+++ src/main/java/com/eg/weixinchatlog/util/UuidUtil.java
@@ -0,0 +1,12 @@
+package com.eg.weixinchatlog.util;
+
+import java.util.UUID;
+
+/**
+ * @time 2020-01-22 16:44
+ */
+public class UuidUtil {
+    public static String getUuid() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+}
--- src/main/java/com/eg/weixinchatlog/util/VoiceUtil.java
+++ src/main/java/com/eg/weixinchatlog/util/VoiceUtil.java
@@ -1,58 +1,79 @@
 package com.eg.weixinchatlog.util;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.SystemUtils;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 支持在windows系统下，amr、aud、slk、silk转成mp3格式
+ * 语音工具类
+ * silk转pcm和mp3格式
  *
  * @time 2020-02-16 16:01
  */
-public class AudioConvertUtil {
+public class VoiceUtil {
+
     /**
-     * 调用sile_v3_decoder.exe
+     * 调用sile_v3_decoder
      * 将silk转成pcm格式
      *
      * @param source
      * @param target
      */
     public static void silkToPcm(File source, File target) {
+        //因为sile_v3_decoder不能承受很长路径
+        //先将源文件复制到temp目录下，转码后，再复制回去
+        String tempFileName = UuidUtil.getUuid();
+        String tempPath = "D:\\";
+        File tempSource = new File(tempPath, tempFileName);
+        File tempTarget = new File(tempPath, tempFileName + "_result");
+        try {
+            //复制源文件到临时目录
+            FileUtils.copyFile(source, tempSource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //转码：silk转pcm
         List<String> commend = new ArrayList<>();
-        String silk_v3_decoder_path = AudioConvertUtil.class.getResource(
+        String silk_v3_decoder_path = VoiceUtil.class.getResource(
                 "/silk_v3_decoder.exe").getPath();
         commend.add(silk_v3_decoder_path);
-        String sourcePath = source.getAbsolutePath();
-        sourcePath = sourcePath.replace("\\", "/");
-        commend.add(sourcePath);
-        String targetPath = target.getAbsolutePath();
-        targetPath = targetPath.replace("\\", "/");
-        commend.add(targetPath);
+        commend.add(tempSource.getAbsolutePath());
+        commend.add(tempTarget.getAbsolutePath());
         try {
             ProcessBuilder builder = new ProcessBuilder();
             builder.command(commend);
             Process process = builder.start();
-            process.waitFor();
-            //取得命令结果的输出流
-            InputStream fis = process.getInputStream();
-            //用一个读输出流类去读
-            InputStreamReader isr = new InputStreamReader(fis, "gbk");
-            //用缓冲器读行
-            BufferedReader br = new BufferedReader(isr);
+//            process.waitFor();
+            InputStream inputStream = process.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "gbk"));
             String line;
-            //直到读完为止
-            while ((line = br.readLine()) != null) {
+            while ((line = bufferedReader.readLine()) != null) {
                 System.out.println(line);
             }
+            //转码完成
+            //把sile_v3_decoder停下来
+            Runtime.getRuntime().exec("taskkill -f -t -im silk_v3_decoder.exe");
+            //把临时转码结果复制回去
+            FileUtils.copyFile(tempTarget, target);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        //删除临时文件
+//        tempSource.delete();
+//        tempTarget.delete();
     }
 
-    // 调用ffmpeg，pcm转mp3
+    /**
+     * 使用ffmpeg
+     * pcm转mp3
+     *
+     * @param source
+     * @param target
+     */
     public static void pcmToMp3(File source, File target) {
         //ffmpeg -y -f s16le -ar 24000 -ac 1 -i 源文件 目标文件
         List<String> commend = new ArrayList<>();
@@ -89,11 +110,12 @@ public class AudioConvertUtil {
     }
 
     public static void main(String[] args) {
-//        File silk = new File("D:\\Programming\\SoftTopics\\2020.02.15-weixin-decrypt\\phone-data\\weixin\\MicroMsg\\df062cf66082a3f0e79b4f21d7ffed21\\voice2\\14\\99\\msg_4819560215207813c759f0d103.amr");
-        File silk = new File("C:\\Users\\msg_4819560215207813c759f0d103.amr");
+        File silk = new File("D:\\Programming\\SoftTopics\\2020.02.15-weixin-decrypt\\phone-data\\weixin\\MicroMsg\\df062cf66082a3f0e79b4f21d7ffed21\\voice2\\14\\99\\msg_4819560215207813c759f0d103.amr");
+
         File pcm = new File(silk.getParent(), silk.getName() + ".pcm");
         File mp3 = new File(silk.getParent(), silk.getName() + ".mp3");
         silkToPcm(silk, pcm);
         pcmToMp3(pcm, mp3);
+        System.out.println(SystemUtils.JAVA_IO_TMPDIR);
     }
 }
